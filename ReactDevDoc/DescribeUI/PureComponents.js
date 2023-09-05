00eaac8ad97eaab8813f6265ff481ac2a3e99bb8
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-05 11:36:14 +0800

    Update PureComponents.js

--- ReactDevDoc/DescribeUI/PureComponents.js
+++ ReactDevDoc/DescribeUI/PureComponents.js
@@ -1,1 +1,14 @@
-
+export default function Clock({ time }) {
+  let hours = time.getHours();
+  let mode = '';
+  if (hours >= 0 && hours <= 6) {
+    mode = 'night';
+  } else {
+    mode = 'day';
+  }
+  return (
+    <h1 className={mode}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
