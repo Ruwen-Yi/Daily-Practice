85d1b78435cc2203c3c801a2441b477d6e6567f3
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-07 20:48:26 +0800

    Update series_of_state_update.js

--- ReactDevDoc/Adding_Interactivity/series_of_state_update.js
+++ ReactDevDoc/Adding_Interactivity/series_of_state_update.js
@@ -31,3 +31,22 @@ function delay(ms) {
     setTimeout(resolve, ms);
   });
 }
+
+
+// challenge 2
+export function getFinalState(baseState, queue) {
+  let finalState = baseState;
+
+  for (let update of queue) {
+    if (typeof update === 'function') {
+      // 调用更新函数
+      finalState = update(finalState);
+    } else {
+      // 替换下一个 state
+      finalState = update;
+    }
+  }
+
+  return finalState;
+}
+
