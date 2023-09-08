393e8da03f6e2f453c8acb31a6445f928402b673
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-08 21:50:43 +0800

    Update 1.react_to_input_with_state.js

--- ReactDevDoc/Managing_State/1.react_to_input_with_state.js
+++ ReactDevDoc/Managing_State/1.react_to_input_with_state.js
@@ -29,3 +29,39 @@ export default function Picture() {
     </div>
   );
 }
+
+// challenge 2
+import {useState} from 'react';
+export default function EditProfile() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [fstName, setFstName] = useState('Jane');
+  const [lstName, setLstName] = useState('Jacobs');
+
+
+  function onFstNameChange(e){
+    setFstName(e.target.value);
+  }
+  function onLstNameChange(e){
+    setLstName(e.target.value);
+  }
+
+  return (
+    <form>
+      <label>
+        First name:{' '}
+        {isEditing ? (<input value={fstName} onChange={onFstNameChange}/>) : (<b>{fstName}</b>)}
+      </label>
+      <label>
+        Last name:{' '}
+        {isEditing ? (<input value={lstName} onChange={onLstNameChange}/>) : (<b>{lstName}</b>)}
+        
+      </label>
+      <button type="submit" onClick={(e)=>{
+      e.preventDefault();
+      setIsEditing(!isEditing)}}>
+        {isEditing ? 'Save':'Edit'} Profile
+      </button>
+      <p><i>Hello, {fstName} {lstName}!</i></p>
+    </form>
+  );
+}
