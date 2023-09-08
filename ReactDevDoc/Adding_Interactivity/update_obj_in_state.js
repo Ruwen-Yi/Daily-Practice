c6d5d9e45e37a9e5b5538ba5289f363515e3db64
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-08 12:14:43 +0800

    Update update_obj_in_state.js

--- ReactDevDoc/Adding_Interactivity/update_obj_in_state.js
+++ ReactDevDoc/Adding_Interactivity/update_obj_in_state.js
@@ -1,1 +1,11 @@
-
+// replace the obj in state instead of mutating it
+const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+});
+let onPointerMove = (e)=> {
+    setPosition({       // 传入一个新对象，替代旧的state值
+      x: e.clientX,
+      y: e.clientY
+    });
+}
