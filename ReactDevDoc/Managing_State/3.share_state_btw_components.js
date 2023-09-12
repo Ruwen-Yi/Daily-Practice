00ab878afb21e90e624dba9b8237ed216a319eb5
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-12 16:34:02 +0800

    Create 3.share_state_btw_components.js

---
+++ ReactDevDoc/Managing_State/3.share_state_btw_components.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function SyncedInputs() {
+  const [text, setText] = useState('');
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <>
+      <Input
+        label="第一个输入框"
+        value={text}
+        onChange={handleChange}
+      />
+      <Input
+        label="第二个输入框"
+        value={text}
+        onChange={handleChange}
+      />
+    </>
+  );
+}
+
+function Input({ label, value, onChange }) {
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
