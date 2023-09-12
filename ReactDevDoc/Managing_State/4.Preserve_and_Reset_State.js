d7077a6674071f5c1c1e2dd8d334e0fbd86543a2
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-12 18:55:16 +0800

    Update 4.Preserve_and_Reset_State.js

--- ReactDevDoc/Managing_State/4.Preserve_and_Reset_State.js
+++ ReactDevDoc/Managing_State/4.Preserve_and_Reset_State.js
@@ -1,4 +1,4 @@
-// challenge (wrong)
+// challenge 1 (wrong)
 import { useState } from 'react';
 
 export default function App() {
@@ -105,5 +105,63 @@ function Form() {
     />
   );
 }
+
+
+
+
+// challenge 4
+import { useState } from 'react';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const hasNext = index < images.length - 1;
+
+  function handleClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  let image = images[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        下一张
+      </button>
+      <h3>
+        {images.length} 张图片中的第 {index + 1} 张
+      </h3>
+      <img key={image.src} src={image.src} />    {/* add key for each img*/}
+      <p>
+        {image.place}
+      </p>
+    </>
+  );
+}
+
+let images = [{
+  place: 'Penang, Malaysia',
+  src: 'https://i.imgur.com/FJeJR8M.jpg'
+}, {
+  place: 'Lisbon, Portugal',
+  src: 'https://i.imgur.com/dB2LRbj.jpg'
+}, {
+  place: 'Bilbao, Spain',
+  src: 'https://i.imgur.com/z08o2TS.jpg'
+}, {
+  place: 'Valparaíso, Chile',
+  src: 'https://i.imgur.com/Y3utgTi.jpg'
+}, {
+  place: 'Schwyz, Switzerland',
+  src: 'https://i.imgur.com/JBbMpWY.jpg'
+}, {
+  place: 'Prague, Czechia',
+  src: 'https://i.imgur.com/QwUKKmF.jpg'
+}, {
+  place: 'Ljubljana, Slovenia',
+  src: 'https://i.imgur.com/3aIiwfm.jpg'
+}];
 
 
