495f1495acfb7886066b9c34415ab507d02d69ef
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-12 17:59:53 +0800

    Create 4.Preserve_and_Reset_State.js

---
+++ ReactDevDoc/Managing_State/4.Preserve_and_Reset_State.js
@@ -0,0 +1,109 @@
+// challenge (wrong)
+import { useState } from 'react';
+
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  if (showHint) {
+    return (
+      <div>
+        <p><i>提示：你最喜欢的城市？</i></p>
+        <Form />
+        <button onClick={() => {
+          setShowHint(false);
+        }}>隐藏提示</button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Form />
+      <button onClick={() => {
+        setShowHint(true);
+      }}>显示提示</button>
+    </div>
+  );
+}
+
+function Form() {
+  const [text, setText] = useState('');
+  return (
+    <textarea
+      value={text}
+      onChange={e => setText(e.target.value)}
+    />
+  );
+}
+
+// challenge 1 (1)
+import { useState } from 'react';
+
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  return (
+    <div>
+      {showHint &&
+        <p><i>提示：你最喜欢的城市？</i></p>
+      }
+      <Form />
+      {showHint ? (
+        <button onClick={() => {
+          setShowHint(false);
+        }}>隐藏提示</button>
+      ) : (
+        <button onClick={() => {
+          setShowHint(true);
+        }}>显示提示</button>
+      )}
+    </div>
+  );
+}
+
+function Form() {
+  const [text, setText] = useState('');
+  return (
+    <textarea
+      value={text}
+      onChange={e => setText(e.target.value)}
+    />
+  );
+}
+
+
+// challenge 1 (2)
+import { useState } from 'react';
+
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  if (showHint) {
+    return (
+      <div>
+        <p><i>提示：你最喜欢的城市？</i></p>
+        <Form />
+        <button onClick={() => {
+          setShowHint(false);
+        }}>隐藏提示</button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {null}
+      <Form />
+      <button onClick={() => {
+        setShowHint(true);
+      }}>显示提示</button>
+    </div>
+  );
+}
+
+function Form() {
+  const [text, setText] = useState('');
+  return (
+    <textarea
+      value={text}
+      onChange={e => setText(e.target.value)}
+    />
+  );
+}
+
+
