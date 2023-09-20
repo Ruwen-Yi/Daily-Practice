5ee2d1b9a496209ac32ae51b7607d24ae0642425
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-20 17:18:27 +0800

    Update 1.useRef.js

--- ReactDevDoc/Escape Hatches/1.useRef.js
+++ ReactDevDoc/Escape Hatches/1.useRef.js
@@ -55,3 +55,43 @@ export default function Toggle() {
     </button>
   );
 }
+
+
+// challenge 3
+import { useRef } from 'react';
+
+function DebouncedButton({ onClick, children }) {
+  const timeoutRef = useRef(null);
+  return (
+    <button onClick={() => {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = setTimeout(() => {
+        onClick();
+      }, 1000);
+    }}>
+      {children}
+    </button>
+  );
+}
+
+export default function Dashboard() {
+  return (
+    <>
+      <DebouncedButton
+        onClick={() => alert('宇宙飞船已发射！')}
+      >
+        发射宇宙飞船
+      </DebouncedButton>
+      <DebouncedButton
+        onClick={() => alert('汤煮好了！')}
+      >
+        煮点儿汤
+      </DebouncedButton>
+      <DebouncedButton
+        onClick={() => alert('摇篮曲唱完了！')}
+      >
+        唱首摇篮曲
+      </DebouncedButton>
+    </>
+  )
+}
