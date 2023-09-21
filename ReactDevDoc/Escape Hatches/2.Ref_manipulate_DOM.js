16ed958127a530136dda971b7f30a7cc5340e6e5
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-21 18:13:06 +0800

    Update 2.Ref_manipulate_DOM.js

--- ReactDevDoc/Escape Hatches/2.Ref_manipulate_DOM.js
+++ ReactDevDoc/Escape Hatches/2.Ref_manipulate_DOM.js
@@ -58,3 +58,147 @@ export default function Page() {
     </>
   );
 }
+
+
+
+// challenge 3 (my answer)
+import { useState, useRef } from 'react';
+
+export default function CatFriends() {
+  const [index, setIndex] = useState(0);
+  const listRef = useRef(new Map());
+
+  function refCallback(node,id){
+    let map = listRef.current;
+    if (node) {
+      map.set(id, node);
+    }
+    else {
+      map.delete(id);
+    }
+  }
+
+  function scrollToView(index){
+    let currentPic = listRef.current.get(index);
+    currentPic.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    });
+  }
+  
+  return (
+    <>
+      <nav>
+        <button onClick={() => {
+          // if (index < catList.length - 1) {
+          //   setIndex(index + 1);
+          //   scrollToView(index+1);
+          // } else {
+          //   setIndex(0);
+          //   scrollToView(0);
+          // }
+          let nextIndex = (index + 1) % catList.length
+          setIndex(nextIndex);
+          scrollToView(nextIndex)
+        }}>
+          下一个
+        </button>
+      </nav>
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li 
+              key={cat.id}
+              ref={(node)=>refCallback(node, cat.id)}
+              >
+              <img
+                className={
+                  index === i ?
+                    'active' :
+                    ''
+                }
+                src={cat.imageUrl}
+                alt={'猫猫 #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://placekitten.com/250/200?image=' + i
+  });
+}
+
+
+
+// challenge 3 (react dev answer)
+import { useRef, useState } from 'react';
+import { flushSync } from 'react-dom';
+
+export default function CatFriends() {
+  const selectedRef = useRef(null);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => {
+          flushSync(() => {
+            if (index < catList.length - 1) {
+              setIndex(index + 1);
+            } else {
+              setIndex(0);
+            }
+          });
+          selectedRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'center'
+          });            
+        }}>
+          下一步
+        </button>
+      </nav>
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li
+              key={cat.id}
+              ref={index === i ?
+                selectedRef :
+                null
+              }
+            >
+              <img
+                className={
+                  index === i ?
+                    'active'
+                    : ''
+                }
+                src={cat.imageUrl}
+                alt={'猫猫 #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://placekitten.com/250/200?image=' + i
+  });
+}
+
