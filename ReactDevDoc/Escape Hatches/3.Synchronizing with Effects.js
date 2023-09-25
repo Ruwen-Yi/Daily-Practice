d50ea596817ffeec501da214a7db38faa1060156
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-25 16:28:33 +0800

    Update 3.Synchronizing with Effects.js

--- ReactDevDoc/Escape Hatches/3.Synchronizing with Effects.js
+++ ReactDevDoc/Escape Hatches/3.Synchronizing with Effects.js
@@ -38,3 +38,21 @@ export default function MyInput({ shouldFocus, value, onChange }) {
     />
   );
 }
+
+// challenge 3
+import { useState, useEffect } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    function onTick() {
+      setCount(c => c + 1);
+    }
+
+    const intervalId = setInterval(onTick, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <h1>{count}</h1>;
+}
