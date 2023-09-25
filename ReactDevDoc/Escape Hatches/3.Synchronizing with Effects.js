7756ec3f4127b9b0b0c11508ce266c3cacd34bfb
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-25 16:56:25 +0800

    Update 3.Synchronizing with Effects.js

--- ReactDevDoc/Escape Hatches/3.Synchronizing with Effects.js
+++ ReactDevDoc/Escape Hatches/3.Synchronizing with Effects.js
@@ -56,3 +56,39 @@ export default function Counter() {
 
   return <h1>{count}</h1>;
 }
+
+
+// challenge 4
+import { useState, useEffect } from 'react';
+import { fetchBio } from './api.js';
+
+export default function Page() {
+  const [person, setPerson] = useState('Alice');
+  const [bio, setBio] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    setBio(null);
+    fetchBio(person).then(result => {
+      if (!ignore) {
+        setBio(result);
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, [person]);
+
+  return (
+    <>
+      <select value={person} onChange={e => {
+        setPerson(e.target.value);
+      }}>
+        <option value="Alice">Alice</option>
+        <option value="Bob">Bob</option>
+        <option value="Taylor">Taylor</option>
+      </select>
+      <hr />
+      <p><i>{bio ?? '加载中……'}</i></p>
+    </>
+  );
+}
