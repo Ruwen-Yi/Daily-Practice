50f3d7c50a5de6f45dc5dc5fcf6abe62a0246975
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-25 14:40:45 +0800

    Update 3.Synchronizing with Effects.js

--- ReactDevDoc/Escape Hatches/3.Synchronizing with Effects.js
+++ ReactDevDoc/Escape Hatches/3.Synchronizing with Effects.js
@@ -16,3 +16,25 @@ export default function MyInput({ value, onChange }) {
     />
   );
 }
+
+
+// challenge 2
+import { useEffect, useRef } from 'react';
+
+export default function MyInput({ shouldFocus, value, onChange }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (shouldFocus) {
+      ref.current.focus();
+    }
+  }, [shouldFocus]);
+
+  return (
+    <input
+      ref={ref}
+      value={value}
+      onChange={onChange}
+    />
+  );
+}
