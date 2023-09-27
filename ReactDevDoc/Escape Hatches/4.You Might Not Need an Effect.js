0b068fa0f1e0c6b3799e0a8a15b37f75ee10b2aa
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-27 15:25:51 +0800

    Update 4.You Might Not Need an Effect.js

--- ReactDevDoc/Escape Hatches/4.You Might Not Need an Effect.js
+++ ReactDevDoc/Escape Hatches/4.You Might Not Need an Effect.js
@@ -120,3 +120,75 @@ function NewTodo({ onAdd }) {
   );
 }
 
+
+
+
+// challenge 2 
+
+//  // App.js
+import { useState, useMemo } from 'react';
+import { initialTodos, createTodo, getVisibleTodos } from './todos.js';
+
+export default function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [showActive, setShowActive] = useState(false);
+  const [text, setText] = useState('');
+  const visibleTodos = useMemo(
+    () => getVisibleTodos(todos, showActive),
+    [todos, showActive]
+  );
+
+  function handleAddClick() {
+    setText('');
+    setTodos([...todos, createTodo(text)]);
+  }
+
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={showActive}
+          onChange={e => setShowActive(e.target.checked)}
+        />
+        只显示未完成的事项
+      </label>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={handleAddClick}>
+        添加
+      </button>
+      <ul>
+        {visibleTodos.map(todo => (
+          <li key={todo.id}>
+            {todo.completed ? <s>{todo.text}</s> : todo.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+//  // todo.js
+let nextId = 0;
+let calls = 0;
+
+export function getVisibleTodos(todos, showActive) {
+  console.log(`getVisibleTodos() 被调用了 ${++calls} 次`);
+  const activeTodos = todos.filter(todo => !todo.completed);
+  const visibleTodos = showActive ? activeTodos : todos;
+  return visibleTodos;
+}
+
+export function createTodo(text, completed = false) {
+  return {
+    id: nextId++,
+    text,
+    completed
+  };
+}
+
+export const initialTodos = [
+  createTodo('买苹果', true),
+  createTodo('买橘子', true),
+  createTodo('买胡萝卜'),
+];
