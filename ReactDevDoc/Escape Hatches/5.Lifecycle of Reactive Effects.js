aacc19633ce8c79b173031da80cf5345ce79904d
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-27 19:20:28 +0800

    Update 5.Lifecycle of Reactive Effects.js

--- ReactDevDoc/Escape Hatches/5.Lifecycle of Reactive Effects.js
+++ ReactDevDoc/Escape Hatches/5.Lifecycle of Reactive Effects.js
@@ -172,3 +172,153 @@ export default function ChatRoom({ roomId, isEncrypted }) {
   return <h1>欢迎来到 {roomId} 聊天室！</h1>;
 }
 
+
+// challenge 5
+// There are two independent synchronization processes:
+// - The first select box is synchronized to the remote list of planets.
+// - The second select box is synchronized to the remote list of places for the current planetId.
+// This is why it makes sense to describe them as two separate Effects. 
+import { useState, useEffect } from 'react';
+import { fetchData } from './api.js';
+
+export default function Page() {
+  const [planetList, setPlanetList] = useState([])
+  const [planetId, setPlanetId] = useState('');
+
+  const [placeList, setPlaceList] = useState([]);
+  const [placeId, setPlaceId] = useState('');
+
+  useEffect(() => {
+    let ignore = false;
+    fetchData('/planets').then(result => {
+      if (!ignore) {
+        console.log('获取了一个行星列表。');
+        setPlanetList(result);
+        setPlanetId(result[0].id); // 选择第一个行星。
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (planetId === '') {
+      // 第一个选择框还没有选中任何内容。
+      return;
+    }
+
+    let ignore = false;
+    fetchData('/planets/' + planetId + '/places').then(result => {
+      if (!ignore) {
+        console.log('Fetched a list of places on "' + planetId + '".');
+        setPlaceList(result);
+        setPlaceId(result[0].id); // 选择第一个地点
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, [planetId]);
+
+  return (
+    <>
+      <label>
+        选择一个行星：{' '}
+        <select value={planetId} onChange={e => {
+          setPlanetId(e.target.value);
+        }}>
+          {planetList?.map(planet =>
+            <option key={planet.id} value={planet.id}>{planet.name}</option>
+          )}
+        </select>
+      </label>
+      <label>
+        选择一个地点：{' '}
+        <select value={placeId} onChange={e => {
+          setPlaceId(e.target.value);
+        }}>
+          {placeList?.map(place =>
+            <option key={place.id} value={place.id}>{place.name}</option>
+          )}
+        </select>
+      </label>
+      <hr />
+      <p>你将要前往：{planetId || '...'} 的 {placeId || '...'} </p>
+    </>
+  );
+}
+
+// challenge 5 alternative answer
+// to reduce repetition, 
+// you can extract some logic into a custom Hook like useSelectOptions below:
+// App.js
+import { useState } from 'react';
+import { useSelectOptions } from './useSelectOptions.js';
+
+export default function Page() {
+  const [
+    planetList,
+    planetId,
+    setPlanetId
+  ] = useSelectOptions('/planets');
+
+  const [
+    placeList,
+    placeId,
+    setPlaceId
+  ] = useSelectOptions(planetId ? `/planets/${planetId}/places` : null);
+
+  return (
+    <>
+      <label>
+        Pick a planet:{' '}
+        <select value={planetId} onChange={e => {
+          setPlanetId(e.target.value);
+        }}>
+          {planetList?.map(planet =>
+            <option key={planet.id} value={planet.id}>{planet.name}</option>
+          )}
+        </select>
+      </label>
+      <label>
+        Pick a place:{' '}
+        <select value={placeId} onChange={e => {
+          setPlaceId(e.target.value);
+        }}>
+          {placeList?.map(place =>
+            <option key={place.id} value={place.id}>{place.name}</option>
+          )}
+        </select>
+      </label>
+      <hr />
+      <p>You are going to: {placeId || '...'} on {planetId || '...'} </p>
+    </>
+  );
+}
+
+// useSelectOptions.js
+import { useState, useEffect } from 'react';
+import { fetchData } from './api.js';
+
+export function useSelectOptions(url) {
+  const [list, setList] = useState(null);
+  const [selectedId, setSelectedId] = useState('');
+  useEffect(() => {
+    if (url === null) {
+      return;
+    }
+
+    let ignore = false;
+    fetchData(url).then(result => {
+      if (!ignore) {
+        setList(result);
+        setSelectedId(result[0].id);
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, [url]);
+  return [list, selectedId, setSelectedId];
+}
