9a267c6414456c4196afcf73c6b1802ab3d18264
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-09-29 21:24:43 +0800

    Update 6.Separating Events from Effects.js

--- ReactDevDoc/Escape Hatches/6.Separating Events from Effects.js
+++ ReactDevDoc/Escape Hatches/6.Separating Events from Effects.js
@@ -37,3 +37,55 @@ export default function Timer() {
     </>
   );
 }
+
+
+// challenge 2
+
+// Every change to increment causes the Effect to re-synchronize, 
+// which causes the interval to clear. 
+// If you keep clearing the interval every time before it has a chance to fire, 
+// it will appear as if the timer has stalled.
+// 
+// To solve the issue, extract an onTick Effect Event from the Effect
+// Since onTick is an Effect Event, the code inside it isn’t reactive. 
+// The change to increment does not trigger any Effects.
+import { useState, useEffect } from 'react';
+import { experimental_useEffectEvent as useEffectEvent } from 'react';
+
+export default function Timer() {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const onTick = useEffectEvent(() => {
+    setCount(c => c + increment);
+  });
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      onTick();
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Counter: {count}
+        <button onClick={() => setCount(0)}>Reset</button>
+      </h1>
+      <hr />
+      <p>
+        Every second, increment by:
+        <button disabled={increment === 0} onClick={() => {
+          setIncrement(i => i - 1);
+        }}>–</button>
+        <b>{increment}</b>
+        <button onClick={() => {
+          setIncrement(i => i + 1);
+        }}>+</button>
+      </p>
+    </>
+  );
+}
