b5afae3f3e901775a46e43d1fe5258ba09a34d20
Ruwen-Yi <85861768+Ruwen-Yi@users.noreply.github.com>
2023-10-06 17:16:09 +0800

    Update 7. Removing Effect Dependencies.js

--- ReactDevDoc/Escape Hatches/7. Removing Effect Dependencies.js
+++ ReactDevDoc/Escape Hatches/7. Removing Effect Dependencies.js
@@ -18,3 +18,68 @@ export default function Timer() {
 
   return <h1>计数器: {count}</h1>
 }
+
+
+
+// challenge 2
+import { useState, useEffect, useRef } from 'react';
+import { FadeInAnimation } from './animation.js';
+import { experimental_useEffectEvent as useEffectEvent } from 'react';
+
+function Welcome({ duration }) {
+  const ref = useRef(null);
+
+  const onAppear = useEffectEvent(animation => {
+    animation.start(duration);
+  });
+
+  useEffect(() => {
+    const animation = new FadeInAnimation(ref.current);
+    onAppear(animation);
+    return () => {
+      animation.stop();
+    };
+  }, []);
+
+  return (
+    <h1
+      ref={ref}
+      style={{
+        opacity: 0,
+        color: 'white',
+        padding: 50,
+        textAlign: 'center',
+        fontSize: 50,
+        backgroundImage: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)'
+      }}
+    >
+      欢迎
+    </h1>
+  );
+}
+
+export default function App() {
+  const [duration, setDuration] = useState(1000);
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <label>
+        <input
+          type="range"
+          min="100"
+          max="3000"
+          value={duration}
+          onChange={e => setDuration(Number(e.target.value))}
+        />
+        <br />
+        淡入 interval: {duration} ms
+      </label>
+      <button onClick={() => setShow(!show)}>
+        {show ? '移除' : '显示'}
+      </button>
+      <hr />
+      {show && <Welcome duration={duration} />}
+    </>
+  );
+}
